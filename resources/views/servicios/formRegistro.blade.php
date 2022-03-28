004575284e8777fff54072792e1293e3e675e4a1
patty pr <prpatriciam@gmail.com>
2022-03-28 10:49:06 -0400

    serv

--- resources/views/servicios/formRegistro.blade.php
+++ resources/views/servicios/formRegistro.blade.php
@@ -11,7 +11,7 @@
 @section('content')
 
 
-        </div>
+  
         <div class="modal-body">
             <div class="col-sm-12 col-md-12 col-xl-12 card m-auto">
 
@@ -322,6 +322,253 @@
                     </div> 
             
 @stop
-@section('js')
 
+@section('js')
+    <script> 
+    $(document).ready(function () {
+          
+//cargar combo tipo tramite
+    tipo_tramite();
+    
+    function tipo_tramite() {
+        $('#tipo_servicio').empty();
+        var option1 = '<option value="0">SELECCIONAR</option>';
+        $('#tipo_servicio').append(option1);
+        $.ajax({
+            headers: {
+                // 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                'Content-Type': 'application/json'
+            },
+            url: "https://multiservdev.cochabamba.bo/api/v1/cementerio/get-services",
+            method: 'GET',
+            dataType: 'json',
+            data: {},
+            success: function(data) {
+
+                $.each(data.response, function(key, value) {
+                    var option = '<option value="' + value.cuenta + '">' + value.descripcion +
+                        '</option>';
+                    $('#tipo_servicio').append(option);
+                });
+            }
+        });
+    }
+
+
+
+    // cargar combo 2  hijo servcio
+
+    function servicio() {
+        $('#servicio').empty();
+        var option1 = '<option value="0">SELECCIONAR</option>';
+        $('#servicio').append(option1);
+        var valor = $('#tipo_servicio option:selected').val();
+        var permitido=0;
+        $.ajax({
+            headers: {
+                // 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                'Content-Type': 'application/json'
+            },
+            url: "https://multiservdev.cochabamba.bo/api/v1/cementerio/get-services/" + valor + "",
+            method: 'GET',
+            dataType: 'json',
+            data: {},
+            success: function(data) {
+
+                $.each(data.response, function(key, value) {
+                    if(valor=="15224350"){permitido= 1;}
+                     else if(valor=="15224150"){
+                         if(value.cuenta== "15224151"){permitido= 1;} else{permitido=0;}
+                     }
+                    else 
+                    if(valor=="15141020"){
+                        if(value.cuenta== "15141022"){permitido= 1;} else{permitido=0;}
+
+                    }
+                    else
+                     if(valor=="15224200"){
+                        if(value.cuenta== "15224203" || value.cuenta== "15224201"  ){permitido= 1;} else{permitido=0;}
+
+                    }
+                    
+   
+
+              //  if(permitido==0){
+                    
+                    var option = '<option value="' + value.num_sec + '">' + value.cuenta + "-" +
+                                        value.descripcion + "-" + value.monto1 + '</option>';
+                                    $('#servicio').append(option);
+              //  }
+
+                   /* var option = '<option value="' + value.num_sec + '">' + value.cuenta + "-" +
+                        value.descripcion + "-" + value.monto1 + '</option>';
+                    $('#servicio').append(option);*/
+                });
+            }
+        });
+    }
+
+    $(document).on('change', '#tipo_servicio', function() {
+        $('#monto_box').hide();
+        $('#precio_box').hide();
+        $('#cantidad_box').hide();
+        $('#unidad_box').hide();
+        $('#tiempo_box').hide();
+        $('#info_box').hide();
+        $('#gestion_box').hide();
+
+        
+        $('#cantidad_b').val(1);
+        $('#unidad_b').val('Glb');
+        $('#precio_b').val('');
+        $('#monto_b').val('');
+        $('#info_pago').val('');
+        $('#gestion_b').val('');
+
+
+
+        $('#service').show();
+        servicio();
+ 
+    });
+    
+    $(document).on('change', '#servicio', function() {
+        $('#monto_box').show();
+        $('#btn_add').show();
+      
+        $('#cantidad_b').val(1);
+        $('#unidad_b').val('Glb');
+        $('#precio_b').val('');
+        $('#monto_b').val('');
+        $('#info_pago').val('');
+
+        var valor = $('#servicio option:selected').text();
+        var frac = valor.split('-');
+       
+        if($('#servicio option:selected').val()=="525"){
+            $('#cantidad_box').show();
+            $('#unidad_box').show();   
+            $('#precio_box').show();
+            $('#info_box').show();
+            $('#info_pago').show();
+            $('#gestion_box').show();
+
+            $('#gestion_b').val($('#pag_con').val());
+
+          
+
+            var precio_parcial=parseInt( $('#cantidad_b').val()) * parseInt(frac[2]);
+            $('#precio_b').val(frac[2]);
+
+            $('#monto_b').val(precio_parcial);
+        }
+        else{
+            var precio_parcial=parseInt( $('#cantidad_b').val()) * parseInt(frac[2]);
+            $('#precio_b').val(frac[2]);
+             $('#monto_b').val(frac[2]);
+        }
+       
+
+    });
+
+    $(document).on('click', '#btn_add', function() {
+        var ts = $('#tipo_servicio option:selected').text();
+        var tsv = $('#tipo_servicio option:selected').val();
+
+        var ssa = $('#servicio option:selected').text();
+        var frac = ssa.split('-');
+        var ss = frac[1]; 
+        var ssv = $('#servicio option:selected').val();
+        var ver = 0;
+       // $('#pag_con').val($('#gestion_b').val());
+        var cantidad_serv=$('#cantidad_b').val();
+        var unidad=$('#unidad_b').val();
+        var precio=$('#precio_b').val();
+        var tiempo=$('#tiempo_b').val();
+        var tiempo=$('#gestion_b').val();
+
+
+        
+       
+        $('#grilla').show();
+
+
+        // contar cantidad de servicios
+        // Selector Padre
+        var parent = document.querySelector('#grilla');
+
+        // Cantidad de div
+        var divs = parent.querySelectorAll('div');
+        var cantidad = divs.length;
+        //    cantidad de servicio
+
+        var cant_serv = cantidad / 4;
+        var collection = $(".quitar");
+        collection.each(function(index, value) {
+            var idrow = this.id;
+            dat_ts = idrow.split('-');
+            id_ts = dat_ts[0]; 
+            id_s = dat_ts[1];
+            if (tsv == id_ts && ssv == id_s) {
+                ver = 1;
+            }
+
+        });
+        if (ver == 0) {
+            if(typeof cantidad_serv === 'undefined'){ cantidad_serv=1;}
+            if(typeof unidad === 'undefined'){unidad="glb";}
+            var monto =0;
+           // monto=cantidad_serv*precio;
+           monto=$('#monto_b').val();
+
+         
+var fila =   '<div class="card fila p-4">'+ 
+             '<div class="row card-header" >'+
+                        '<div class="col-sm-2 col-md-2 col-xl-2">Tipo servicio:</div>'+
+                        '<div class="col-sm-10 col-md-4 col-xl-4"><span id="txt_tipo_serv">' +ts + '</span>'+
+                        '<input type="hidden" name="tipo_serv[]" value="' +ts + '"  id="ts-' + tsv +
+                        '" class="tserv form-control" readonly="true" class="form-control" /> </div>' +
+                        '<div class="col-sm-2 col-md-1 col-xl-1">Servicio:</div>'+
+                        '<div class="col-sm-10 col-md-4 col-xl-4"><span id="txt_serv">' + ss +'</span>'+
+                        '<input type="hidden" name="hserv[]" value="' + ss +
+                        '"  id="hs-' + ssv + '" class="hserv form-control" readonly="true" />'+
+                        '<input type="hidden" name="servicio[]" value="' + ssv +
+                        '" class="servicio  form-control" readonly="true" />'+
+                        '</div>'+                       
+               ' </div>'+
+               '<div class="row card-body" >' +
+                        '<div class="col-sm-2 col-md-2 col-xl-2">Cantidad:</div>'+
+                        '<div class="col-sm-4 col-md-4 col-xl-4"><span id="txt_cantidad">' + cantidad_serv + '</span>'+
+                        '<input type="hidden" name="cantidad[]" value="' +cantidad_serv + '"   class="cantidad  form-control" readonly="readonly"  /></div>'+
+
+                        '<div class="col-sm-2 col-md-2 col-xl-2">Unidad:</div>'+
+                        '<div class="col-sm-4 col-md-4 col-xl-4"><span id="txt_unidad">' + unidad + '</span>'+
+                        '<input type="hidden" name="unidad[]" value="' +unidad + '"   class="unidad  form-control" readonly="readonly"  /></div>'+
+
+                        '<div class="col-sm-2 col-md-2 col-xl-2">Precio:</div>'+
+                        '<div class="col-sm-4 col-md-4 col-xl-4"><span id="txt_precio">' + precio + '</span>'+
+                          '<input type="hidden" name="precio_unitario[]" value="' +precio + '"   class="precio  form-control" readonly="readonly"  />'+
+                         '<input type="hidden" name="cuenta[]" value="' + frac[0] +'"  id="cuenta" class="cuenta form-control" readonly="true"  class="form-control" /></div>' +
+                       
+                         '<div class="col-sm-2 col-md-2 col-xl-2">Subtotal:</div>'+
+                         '<div class="col-sm-4 col-md-4 col-xl-4"><span id="txt_subtotal">' + monto + '</span>'+
+                         '<input type="hidden" name="monto[]" value="' +  monto +
+                          '"  id="monto" class="montoserv form-control" readonly="readonly"  class="form-control"/> </div>'+
+
+                          '<div class="col-sm-2 col-md-2 col-xl-2">Ultimo Pago:</div>'+
+                         '<div class="col-sm-4 col-md-4 col-xl-4"><span id="txt_subtotal">' + $('#gestion_b').val() + '</span>'+
+                         '<input type="hidden" name="ultimopago" value="' +  $('#gestion_b').val() +
+                          '"  id="ultimopago" class="ultimopago form-control" readonly="readonly"  class="form-control"/> </div>'+
+                    
+             '</div>'+
+             '<div class="col-sm-12 col-md-1 col-xl-1"><a  href="#" class="btn btn-danger quitar "  id="' + tsv + '-' + ssv +
+                         '" onClick="$(this).parent().parent().remove();"><i class="fas fa-minus-circle"></i></a> </div>'+
+               '</card>';
+            $('#grilla').append(fila);
+        } else {
+            alert("servicio repetido");
+        }
+    });
+});
+    </script>
 @stop
